dd38e599dfbc10fc9b8bc0547dbcd5f5d22f5c2e
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-21 22:38:32 -0700

    add model Type for organizing food

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_053535) do
+ActiveRecord::Schema.define(version: 2018_10_22_053807) do
 
   create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
