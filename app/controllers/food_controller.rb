df7f51215b7ec276bf9b6dc874e7f873666ca5c2
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-10-22 20:21:03 -0700

    add changes to form

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -4,6 +4,21 @@ class FoodController < ApplicationController
   end
 
   def new
+    @food = Food.new
+  end
 
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to food_index_url
+    else
+      redirect_to new_food_url
+    end
+  end
+
+  private
+
+  def food_params
+    params.permit(:name, :description)
   end
 end
